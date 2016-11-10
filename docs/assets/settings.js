5463deb08c2ac391253791ce3003fb9d0121ba88
Heloisa <heloisa.biagi@gmail.com>
2016-11-10 17:26:06 -0200

    Docs assets

---
+++ docs/assets/settings.js
@@ -0,0 +1,26 @@
+var blackNight = blackNight || {};
+
+/** General Settings */
+blackNight.mobileAnimations = false; // enable or disable animations on mobile devices. They're disabled by default due to poor performance.
+
+/** Portfolio */
+blackNight.portfolioLayout = "fitRows"; // Isotope plugin layout type. To see the available options: http://isotope.metafizzy.co/layout-modes.html
+blackNight.popupLoadingMessage = 'Loading image'; // message for the lightbox loading
+blackNight.popupPrevMessage = 'Previous (Left arrow key)'; // message for the lightbox navigation between pictures (previous)
+blackNight.popupNextMessage = 'Next (Right arrow key)'; // message for the lightbox navigation between pictures (next)
+blackNight.popupCounterMessage = 'of'; // message for the lightbox image count (i.e: 1of 5)
+
+/** Owl Carousel */
+blackNight.carouselItems = 1;
+blackNight.carouselItemsDesktop = 1;
+blackNight.carouselItemsDesktopSmall = 1;
+blackNight.carouselItemsTablet = 1;
+blackNight.carouselItemsMobile = 1;
+
+/** Contact Form feedback */
+blackNight.contactFieldNameEmpty = "Please fill in the 'name' field"; // Message to be used at the Contact Form's error message
+blackNight.contactFieldEmailEmpty = "Please fill in the 'email' field"; // Message to be used at the Contact Form's error message
+blackNight.contactFieldMessageEmpty = "Please fill in the 'message' field"; // Message to be used at the Contact Form's error message
+blackNight.contactInvalidEmail = "Invalid Email"; // Message to be used at the Contact Form's error message
+blackNight.contactMessageSent = "Your message has been sent!"; // Success Message to be used at the Contact Form's submission
+blackNight.contactMessageNotSent= "Unable to send email. Please try again later."; // Error Message to be used at the Contact Form's submission
